46cf9f836c0f51b9ae1bdba605c77cca6025b3fe
delmaler <delmaler1@gmail.com>
2021-01-11 13:43:48 +0200

    added basic functions

---
+++ DevStack.py
@@ -0,0 +1,16 @@
+from collections import deque
+from random import shuffle
+
+
+class DevStack:
+    deck = list[
+        "knight", "knight", "knight", "knight", "knight", "knight", "knight", "knight", "knight", "knight", "knight", "knight"
+        , "knight", "knight", "win_point", "win_point", "win_point", "win_point", "win_point", "monopole", "monopole",
+        "2_free_roads", "2_free_roads", "2_free_resources", "2_free_resources"]
+
+    def __init__(self):
+        shuffle(self.deck)
+
+    def get(self):
+        return self.deck.pop
+
--- Player.py
+++ Player.py
@@ -1,5 +1,29 @@
+from enum import Enum
+import DevStack
+class Resource(Enum):
+    WHEAT=1
+    SHEEP=2
+    CLAY=3
+    WOOD=4
+    SILVER=5
+
 class player:
+    resources = {Resource.CLAY: 0, Resource.WOOD: 0, Resource.SHEEP: 0, Resource.SILVER: 0, Resource.WHEAT: 0}
+    devCards = DevStack()
+
     def __init__(self,index,name=None):
         self.index=index
         self.name=name
-    
+        self.resources={Resource.CLAY : 0 , Resource.WOOD : 0, Resource.SHEEP: 0, Resource.SILVER : 0, Resource.WHEAT:0}
+        self.devCards = []
+    def add_resources(resource,number):
+        player.resources[resource]+=number
+    def buy_devops:
+        if(Resource.SHEEP<1 or Resource.SILVER<1 or Resource.WHEAT<1):
+            print("Not enough resources")
+            return
+        player.resources[Resource.SHEEP]-=1
+        player.resources[Resource.SILVER] -= 1
+        player.resources[Resource.WHEAT] -=1
+        player.devCards.append(Game.devStack.get())
+
