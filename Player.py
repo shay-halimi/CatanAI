5f025d6834224205d4602d01f40ce3510aa17437
delmaler <delmaler1@gmail.com>
2021-03-24 22:44:35 +0200

    tweaked DevStack

--- Player.py
+++ Player.py
@@ -1,6 +1,8 @@
 from enum import Enum
-import DevStack
+
+import Game
 from Resources import Resource
+
 
 class Player:
     resources = {Resource.CLAY: 0, Resource.WOOD: 0, Resource.SHEEP: 0, Resource.IRON: 0, Resource.WHEAT: 0}
@@ -37,25 +39,22 @@ class Player:
         return
 
     def buy_City(self):
-        if self.resources[Resource.IRON]<3 or self.resources[Resource.WHEAT]<2:
+        if self.resources[Resource.IRON] < 3 or self.resources[Resource.WHEAT] < 2:
             print("Not enough resources")
             return
         self.resources[Resource.WHEAT] -= 2
         self.resources[Resource.IRON] -= 3
         # TODO need to add the city to the map with the right color
         return
+
     def buy_road(self):
-        if self.resources[Resource.CLAY] < 1 or self.resources[Resource.WOOD]<1:
+        if self.resources[Resource.CLAY] < 1 or self.resources[Resource.WOOD] < 1:
             print("Not enough resources")
             return
         self.resources[Resource.CLAY] -= 1
         self.resources[Resource.WOOD] -= 1
+
     def use_knight(self):
         if "knight" in self.devCards:
             self.devCards.remove("knight")
-            #TODO add a function to move the robber and get one reasource card frrom another player
-
-
-
-
-
+            # TODO add a function to move the robber and get one reasource card frrom another player
