aa056354c7a9871952a5dddec702705c67dadd8c
shay halimi <shayhalimi@gmail.com>
2021-03-25 14:40:42 +0200

    changes to Game

--- Game.py
+++ Game.py
@@ -16,24 +16,30 @@ class Game:
             self.players += [Player(i)]
         self.devStack = DevStack.DevStack()
 
+    # ToDo (shay) : link to API
     def start_game(self):
-        for i in len(self.players):
+        for i in range(len(self.players)):
             settlement_is_valid = False
             while not settlement_is_valid:
+                # ToDo: implement for cp agents as well
                 print("Player", i, "In Which location do you want to build your 1st settlement?")
                 loc_coords = input()
                 # TODO need to check that coord are valid i.e. within the board and not within less than 2 edges
-                #  frrom other settelment
+                #  from other settlement
+                # ToDo (shay) : update the choice in Board (map).
                 settlement_is_valid = True
                 # TODO need to add the settelment to the map
             road_is_valid = False
             while not road_is_valid:
+                # ToDo: implement for cp agents as well
                 print("Player", i, "In Which location do you want to build your road?")
                 road_coords = input()
+                # ToDo (shay) : update the choice in Board (map).
                 # TODO need to check if road location is valid and even let the player choose only 1 from 3 choices
                 # TODO need to add road to the map
                 road_is_valid = True
-        for i in range(len(self.players), 0, -1):
+        for i in range(len(self.players)-1, -1, -1):
+            settlement_is_valid = False
             while not settlement_is_valid:
                 print("Player", i, "In Which location do you want to build your 2nd settlement?")
                 loc_coords = input()
